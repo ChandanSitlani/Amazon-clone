504a7c8de2a4b0359e2ac6b5de4807952b7d30cd
Chandan Sitlani <sitlanic@gmail.com>
2019-08-30 11:24:13 +0100

    added schema

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
 var express=require('express'),
 app=express(),
 bodyParser=require('body-parser');
+app.get('/',function(req,res)
+{
+	res.send("Hello");
+});
+app.listen(3000,function(err)
+{
+	if(!err)
+console.log("serving on 3000");
+});
