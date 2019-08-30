e8692f8322a1ec0b033a5e3cac3f035949aba94f
Chandan Sitlani <sitlanic@gmail.com>
2019-08-30 12:28:31 +0100

    added passport

--- server.js
+++ server.js
@@ -1,18 +1,34 @@
 var express=require('express'),
 app=express(),
 
-localStratergy=require('passport-local'),
+LocalStrategy=require('passport-local'),
+passport=require('passport');
 user=require('./models/user'),
 authRoutes=require('./routes/auth'),
 bodyParser=require('body-parser'),
 mongoose=require('mongoose');;
 
+
+app.use(bodyParser.json({ limit: '50mb' }));
+
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
+
+app.use(passport.initialize());
+
+app.use(passport.session());
+
+passport.use(new LocalStrategy(user.authenticate()));
+
+passport.serializeUser(user.serializeUser());
+
+passport.deserializeUser(user.deserializeUser());
 mongoose.connect('mongodb://localhost:27017/amazon_clone');
 var db=mongoose.connection;
 db.on('error',function(err){console.log(err);});
 db.once('open',function(err,res){
 	console.log("DB Connected");
 });
+
 app.get('/',function(req,res)
 {
 	res.send("Hello");
