aa46b7a90435ef7468543e0a103e80d1f22893b2
Chandan Sitlani <sitlanic@gmail.com>
2019-08-30 11:07:05 +0100

    added schema

---
+++ models/user.js
@@ -0,0 +1,8 @@
+var mongoose=require('mongoose'),
+passportLocalMongoose=require('passport-local-mongoose');
+var userSchema=new mongoose.Schema({
+name:{type:String},
+email:{type:String}
+});
+userSchema.plugin(passportLocalMongoose);
+module.exports=mongoose.model("user",userSchema);
