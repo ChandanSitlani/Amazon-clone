231bc4ed25feb0ef4a93e31b2ca2874ffa15ad79
Chandan Sitlani <sitlanic@gmail.com>
2019-08-30 11:38:14 +0100

    added views

---
+++ routes/auth.js
@@ -0,0 +1,16 @@
+var express=require('express'),
+router=express.Router();
+var user=require('./models/user');
+router.get('/signup',function(req,res)
+{
+user.register(new user({name:req.body.name,email:req.body.email}),req.body.password,function(err,user)
+{
+if(!err)
+{passport.authenticate("local")(req,res,function(){
+res.send("Done");
+});
+});
+});
+module.exports=router;
+
+
